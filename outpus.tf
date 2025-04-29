6930362f5607cc45bc51ccb51b21b762ef278efc
palli-sekhar <areyentiraidhi98@gmail.com>
2025-04-29 15:20:49 +0530

    outpus.tf

--- outpus.tf
+++ outpus.tf
@@ -4,8 +4,3 @@ output "ec2_public_ip" {
   value       = aws_instance.web.public_ip
 }
 
-output "ec2_public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.web.public_ip
-}
-
